45170d6221b074b9f2f367b4dec34cb4f1e4ad32
wub <gitwub@gmail.com>
2019-05-21 11:41:19 +0200

    secondary branch first commit

---
+++ navbar.php
@@ -0,0 +1,18 @@
+<!-- Navbar -->
+<main class="container-expand-fg">
+    <div class="navbar">
+    <ul class="logo">
+            <li><a href="#"><img src="img/placeholder2.png" class="logo2" ></a></li>
+</ul>
+<ul class="nav">
+            <li> <a href="#" class="navFont"> HOME </a></li>
+            <li> <a href="#" class="navFont"> INFO▼ </a></li>
+            <li> <a href="#" class="navFont"> ABOUT </a></li>
+            <li> <a href="#" class="navFont"> CONTACT </a></li>
+            <li> <a href="#" class="navFont"> FAQ </a></li>
+            </ul>
+            <ul class="logo2">
+            <li><a href="#" class="right"><img src="./img/placeholder-man.png" class="logo"></a></li>
+</ul>
+        </div>
+</main>
