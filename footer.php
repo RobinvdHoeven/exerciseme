7de2f040637dcae9bb5f517c6173762426574a13
wub <gitwub@gmail.com>
2019-05-24 09:20:10 +0200

    i forgot

--- footer.php
+++ footer.php
@@ -2,13 +2,22 @@
 <div class="container">
   <div class="row">
     <div class="col-md-12 col-lg-4" id="twoCard">
-      One of three columns
+    <br>
+    <br>
+      <p id="cardTitle">GET INFORMATION</p>
+      <p id="cardSubtext">Go here if you want to know more about exericising >></p>
     </div>
     <div class="col-md-12 col-lg-4" id="twoCard">
-      One of three columns
+    <br>
+    <br>
+      <p id="cardTitle">GET INFORMATION</p>
+      <p id="cardSubtext">Go here if you want to know more about exericising >></p>
     </div>
     <div class="col-md-12 col-lg-4" id="lastCard">
-      One of three columns
+    <br>
+    <br>
+      <p id="cardTitle">GET INFORMATION</p>
+      <p id="cardSubtext">Go here if you want to know more about exericising >></p>
     </div>
   </div>
 </div>
